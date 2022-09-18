4b92bcdab1a7d09e25249d3af15cef25692ae0d2
Antonio Leite Jr <88110285+tleitejr@users.noreply.github.com>
2022-09-17 21:12:38 -0300

    Add files via upload

---
+++ Esporte/_javascript/script.js
@@ -0,0 +1,8 @@
+var botao= document.querySelector("button");
+    
+    botao.addEventListener("click", Menor );
+    
+    function Menor () {
+        
+        var menor = alert('Desculpe, apenas clientes maiores de idade poderão participar das promoções.');
+    }
