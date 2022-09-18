605aa8f6059df7bcab1205cced84261a623315dd
Antonio Leite Jr <88110285+tleitejr@users.noreply.github.com>
2022-09-17 21:12:02 -0300

    Add files via upload

---
+++ PI_site/js/script.js
@@ -0,0 +1,22 @@
+/*$(function () {
+    var menu_width = 290;
+    var menu = $(".menu");
+    var menu_open = $(".menu-open");
+    var menu_close = $(".menu-close");
+    var overlay = $(".overlay");
+   
+    menu_open.click(function (e) {
+      e.preventDefault();
+      menu.css({"right": "0px"});
+      overlay.css({"opacity": "1", "width": "100%"});
+    });
+    
+    menu_close.click(function (e) {
+      e.preventDefault();
+      menu.css({"right": "-" + menu_width + "px"});
+      overlay.css({"opacity": "0", "width": "0"});
+    });
+   });*/
+
+$('.carousel') .carousel()
+
